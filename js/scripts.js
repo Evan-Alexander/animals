08197ba0e3216bfa0eb1cd05a521af785fb7d4f1
Jason Brown <jasontbrown99@gmail.com>
2017-01-12 12:48:59 -0800

    add styling to css, add missing brackets to arguemnts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
   $("form#dog-selection").submit(function(event) {
     var breedItem = $("#breeds").val();
-    if (breedItem === "fox"){
+    if (breedItem === "Smooth-haired Fox Terrier") {
       $("#fox").show();
       $("#doberman").hide();
       $("#boxer").hide();
       $("#b-collie").hide();
-    // } else if (breedItem === "")
-    event.preventDefault();
+    } else if (breedItem === "Doberman Pinscher") {
+      $("#doberman").show();
+      $("#fox").hide();
+      $("#boxer").hide();
+      $("#b-collie").hide();
+    } else if (breedItem === "Boxer") {
+      $("#boxer").show();
+      $("#fox").hide();
+      $("#doberman").hide();
+      $("#b-collie").hide();
+    } else if (breedItem === "Border Collie") {
+      $("#b-collie").show();
+      $("#fox").hide();
+      $("#doberman").hide();
+      $("#boxer").hide();
+    }
+      event.preventDefault();
 
   });
 });
